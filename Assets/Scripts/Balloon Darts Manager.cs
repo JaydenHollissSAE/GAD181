9756e13bcc4ab34a96f9bb20dd54423ed35f48b7
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-07-01 15:54:40 +1000

    Balloons go brrr

---
+++ Assets/Scripts/Balloon Darts Manager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonDartsManager : MonoBehaviour
+{
+    
+    public List<Vector2> rowsStarting = new List<Vector2>();
+    public List<Vector2> rowsEnding = new List<Vector2>();
+    public List<GameObject> balloons = new List<GameObject>();
+    public GameObject balloonHolder;
+    public List<float> balloonSpeed = new List<float>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
