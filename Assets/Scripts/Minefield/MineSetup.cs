4b12c52864a283ddd59d5e8d9f6673e78601c9ff
miloeck <159411519+miloeck@users.noreply.github.com>
2024-07-08 15:01:32 +1000

    Minefield player movement.
    Player can move around a grid. Beginning the creation of mine functionality.

---
+++ Assets/Scripts/Minefield/MineSetup.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSetup : MonoBehaviour
+{
+    public GameObject minePrefab;
+    public GameObject minefieldParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMines();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetUpMines()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            float xPos = Random.Range(-5f, 4f) + 0.5f;
+            float yPos = Random.Range(-5f, 4f) + 0.5f;
+
+            GameObject newMine = Instantiate(minePrefab, minefieldParent.transform);
+            newMine.transform.position = new Vector3(xPos, yPos, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Minefield/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 15f;
+    public Transform movePoint;
+
+    public LayerMask whatIsMines;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) < 0.1f)
+        {
+
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            }
+
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+
+            }
+        }
+
+        Collider2D hit;
+        hit = Physics2D.OverlapCircle(movePoint.position, 0.05f, whatIsMines);
+        if(hit != null)
+        {
+            Debug.Log(hit.gameObject);
+            movePoint.position = new Vector3(4.5f, -4.5f, -1);
+            hit = null;
+        }
+    }
+}
