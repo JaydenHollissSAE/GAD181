7880cb9d1450c6f4451e666f994857d82be9defb
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-07-11 17:08:18 +1000

    Redid Balloon Spawner

--- Assets/Scripts/Balloons/SpawnerBD.cs
+++ Assets/Scripts/Balloons/SpawnerBD.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class SpawnerBD : MonoBehaviour
 {
     public List<GameObject> balloons = new List<GameObject>();
+    [SerializeField] List<Vector2> spawnPositions = new List<Vector2>();
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject b = Instantiate(balloons[Random.Range(0, balloons.Count)]);
+        StartCoroutine(SpawnBalloons());
+        
+    }
+
+    private IEnumerator SpawnBalloons()
+    {
+        GameObject b = Instantiate(balloons[Random.Range(0, balloons.Count)], spawnPositions[Random.Range(0, spawnPositions.Count)], Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.7f));
+        StartCoroutine(SpawnBalloons());
+
     }
 }
