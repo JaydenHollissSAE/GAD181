403b63af852e12c0e7ae46d0e0000eb4f0e63254
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-07-11 17:16:23 +1000

    Readded movement

--- Assets/Scripts/Balloons/BalloonBD.cs
+++ Assets/Scripts/Balloons/BalloonBD.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BalloonBD : MonoBehaviour
 {
     public int value;
+    private Vector2 moveTo;
+    [SerializeField] float moveSpeed;
 
     public void Pop()
     {
         GameManagerBD.inst.pointCallback.Invoke(value);
+    }
+    private void Start()
+    {
+        moveTo = new Vector2(-transform.position.x, transform.position.y);
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveTo, Time.deltaTime*moveSpeed);
     }
 }
