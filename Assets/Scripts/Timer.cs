84314a8125c65ee5073d0d7cd8b0a20adadd6b00
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-07-14 21:30:53 +1000

    Timer Placeholder UI

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,32 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
+    private Image image;
     [SerializeField] List<Sprite> timerSprites = new List<Sprite>();
     private int currentSpriteId;
     public float gameTime;
     private float timePassed;
+    [SerializeField] GameObject timerSprite;
+    [SerializeField] TextMeshProUGUI timerText;
+    private float timeCalc;
 
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        image = timerSprite.GetComponent<Image>();
         StartCoroutine(RunTimer());
     }
 
     private IEnumerator RunTimer()
     {
         yield return new WaitForSeconds(1f);
-
-        if (timePassed >= gameTime / timerSprites.Count * (currentSpriteId+1)) 
+        timeCalc = gameTime / timerSprites.Count * (currentSpriteId + 1);
+        Debug.Log(timeCalc);
+        Debug.Log(timeCalc - (timeCalc / (currentSpriteId + 1)));
+        if (timePassed >= timeCalc - (timeCalc / (currentSpriteId+1))) 
         {
-            spriteRenderer.sprite = timerSprites[currentSpriteId];
+            image.sprite = timerSprites[currentSpriteId];
             currentSpriteId += 1;
         }
         timePassed += 1;
+        timerText.text = (gameTime - timePassed).ToString();
 
 //        yield return new WaitForSeconds(gameTime/timerSprites.Count);
         if (timePassed < gameTime)
