68799156d8a26d446c998ab254dde4c8a741f6f6
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-07-22 13:05:52 +1000

    Fsh go brrrrrr

--- Assets/Scripts/Fishing/Fish.cs
+++ Assets/Scripts/Fishing/Fish.cs
@@ -6,17 +6,29 @@ public class Fish : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     private Vector2 moveTo;
+    [SerializeField] float waitTime;
+    private bool moveBuffer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(MoveFish());
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveTo = new Vector2(Random.Range(transform.position.x - 3, transform.position.x + 3), Random.Range(transform.position.y - 3, transform.position.y + 3));
         transform.position = Vector2.MoveTowards(transform.position, moveTo, Time.deltaTime * moveSpeed);
         Debug.Log(moveTo);
+    }
+
+
+
+    private IEnumerator MoveFish()
+    {
+        moveTo = new Vector2(Random.Range(transform.position.x - 10, transform.position.x + 10), Random.Range(transform.position.y - 10, transform.position.y + 10));
+        Debug.Log("Wait Started");
+        yield return new WaitForSeconds(waitTime);
+        Debug.Log("I waited for you, just like you asked of me");
+        StartCoroutine(MoveFish());
     }
 }
