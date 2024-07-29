3c00db9459c63fe2f224947fb6477065f44ca897
miloeck <159411519+miloeck@users.noreply.github.com>
2024-07-29 11:06:48 +1000

    Started sum mechanic

--- Assets/Scripts/Memory Maths/Sum.cs
+++ Assets/Scripts/Memory Maths/Sum.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Sum : MonoBehaviour
 {
+    public int sumGoal;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateSumGoal();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GenerateSumGoal()
+    {
+        sumGoal = Random.Range(10, 30);
     }
 }
