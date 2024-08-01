4f81de7bea351e15bf5c3d9285a5ad2a84b9d775
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-08-01 13:54:11 +1000

    Tweaked fish spawning, added hook prefab

--- Assets/Scripts/Fishing/Fish.cs
+++ Assets/Scripts/Fishing/Fish.cs
@@ -2,75 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Tilemaps;
 using UnityEngine;
-
-public class Fish : MonoBehaviour
+namespace Fishing
 {
-    [SerializeField] float moveSpeed;
-    private Vector2 moveTo;
-    [SerializeField] float waitTime;
-    private bool moveBuffer;
-    private SpriteRenderer spriteRenderer;
-    [SerializeField] List<Sprite> fishTypes = new List<Sprite>();
-    private float fishSize;
-    // Start is called before the first frame update
-    void Start()
+    public class Fish : MonoBehaviour
     {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        fishSize = Random.Range(0.4f, 3f);
-        moveSpeed = Random.Range(0.5f, 30f);
-        //waitTime = moveSpeed * Random.Range(0.08f, 0.12f);
-        //Debug.Log(Mathf.RoundToInt(100f - fishSize));
-        spriteRenderer.sortingOrder = Mathf.RoundToInt(100f - fishSize);
-        waitTime = Random.Range(0.5f, 1.5f);
-        spriteRenderer.sprite = fishTypes[Random.Range(0, fishTypes.Count-1)];
-        transform.localScale = new Vector2(fishSize, fishSize);
-        StartCoroutine(MoveFish());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //moveTo.x = moveTo.x + Random.Range(-0.003f, 0.003f);
-        //moveTo.y = moveTo.y + Random.Range(-0.003f, 0.003f);
-        transform.position = Vector2.MoveTowards(transform.position, moveTo, Time.deltaTime * moveSpeed);
-        //Debug.Log(moveTo);
-    }
-
-    private void SetMovementPos()
-    {
-        moveTo = new Vector2(Random.Range(transform.position.x - 10, transform.position.x + 10), Random.Range(transform.position.y - 10, transform.position.y + 10));
-        if (moveTo.x > transform.position.x && transform.localScale.x > 0)
+        [SerializeField] float moveSpeed;
+        private Vector2 moveTo;
+        [SerializeField] float waitTime;
+        private bool moveBuffer;
+        private SpriteRenderer spriteRenderer;
+        [SerializeField] List<Sprite> fishTypes = new List<Sprite>();
+        private float fishSize;
+        // Start is called before the first frame update
+        void Start()
         {
-            transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            fishSize = Random.Range(0.4f, 3f);
+            moveSpeed = Random.Range(0.5f, 30f);
+            //waitTime = moveSpeed * Random.Range(0.08f, 0.12f);
+            //Debug.Log(Mathf.RoundToInt(100f - fishSize));
+            spriteRenderer.sortingOrder = Mathf.RoundToInt(100f - fishSize);
+            waitTime = Random.Range(0.5f, 1.5f);
+            spriteRenderer.sprite = fishTypes[Random.Range(0, fishTypes.Count - 1)];
+            transform.localScale = new Vector2(fishSize, fishSize);
+            StartCoroutine(MoveFish());
         }
-        else if (moveTo.x < transform.position.x && transform.localScale.x < 0)
+
+        // Update is called once per frame
+        void Update()
         {
-            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            //moveTo.x = moveTo.x + Random.Range(-0.003f, 0.003f);
+            //moveTo.y = moveTo.y + Random.Range(-0.003f, 0.003f);
+            transform.position = Vector2.MoveTowards(transform.position, moveTo, Time.deltaTime * moveSpeed);
+            //Debug.Log(moveTo);
         }
-        if (moveTo.x > 8 || moveTo.x < -8)
+
+        private void SetMovementPos()
         {
-            while (moveTo.x > 8 || moveTo.x < -8)
+            moveTo = new Vector2(Random.Range(transform.position.x - 10, transform.position.x + 10), Random.Range(transform.position.y - 10, transform.position.y + 10));
+            if (moveTo.x > transform.position.x && transform.localScale.x > 0)
             {
-                moveTo.x = Random.Range(transform.position.x - 10, transform.position.x + 10);
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             }
-            //Debug.Log("Recalculating X");
-        }
-        if (moveTo.y > 4.8 || moveTo.y < -4.8)
-        {
-            while (moveTo.y > 4.8 || moveTo.y < -4.8)
+            else if (moveTo.x < transform.position.x && transform.localScale.x < 0)
             {
-                moveTo.y = Random.Range(transform.position.y - 10, transform.position.y + 10);
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             }
-            //Debug.Log("Recalculating Y");
+            if (moveTo.x > 8 || moveTo.x < -8)
+            {
+                while (moveTo.x > 8 || moveTo.x < -8)
+                {
+                    moveTo.x = Random.Range(transform.position.x - 10, transform.position.x + 10);
+                }
+                //Debug.Log("Recalculating X");
+            }
+            if (moveTo.y > 4.8 || moveTo.y < -4.8)
+            {
+                while (moveTo.y > 4.8 || moveTo.y < -4.8)
+                {
+                    moveTo.y = Random.Range(transform.position.y - 10, transform.position.y + 10);
+                }
+                //Debug.Log("Recalculating Y");
+            }
         }
-    }
 
-    private IEnumerator MoveFish()
-    {
-        SetMovementPos();
-        //Debug.Log("Wait Started");
-        yield return new WaitForSeconds(waitTime);
-        //Debug.Log("I waited for you, just like you asked of me");
-        StartCoroutine(MoveFish());
+        private IEnumerator MoveFish()
+        {
+            SetMovementPos();
+            //Debug.Log("Wait Started");
+            yield return new WaitForSeconds(waitTime);
+            //Debug.Log("I waited for you, just like you asked of me");
+            StartCoroutine(MoveFish());
+        }
     }
 }
--- Assets/Scripts/Fishing/FishingRod.cs
+++ Assets/Scripts/Fishing/FishingRod.cs
@@ -3,43 +3,52 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class FishingRod : MonoBehaviour
+namespace Fishing
 {
-    private Vector2 mousePos;
-    [SerializeField] Camera cam;
-
-    // Start is called before the first frame update
-    void Start()
+    public class FishingRod : MonoBehaviour
     {
+        private Vector2 mousePos;
+        [SerializeField] Camera cam;
+        [SerializeField] GameObject hook;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
+        // Start is called before the first frame update
+        void Start()
         {
-            Vector2 moveTo = new Vector2(hit.point.x, gameObject.transform.position.y);
-            if (moveTo.x < -8)
-            {
-                moveTo.x = -8;
-            }
-            else if (moveTo.x > 8) 
-            { 
-                moveTo.x = 8;
-            }
-            //Debug.Log(moveTo);
-            gameObject.transform.position = Vector2.MoveTowards(transform.position, moveTo, Time.deltaTime * 11.0f);
-            //Debug.Log("Ray Hit");
+
         }
 
-        
+        // Update is called once per frame
+        void Update()
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-        //mousePos = new Vector2(SceneView.currentDrawingSceneView.camera.pixelWidth-Input.mousePosition.x, SceneView.currentDrawingSceneView.camera.pixelHeight - Input.mousePosition.y);
-        //Debug.Log(mousePos);
-        //transform.position = Vector2.MoveTowards(transform.position, new Vector2(mousePos.x, transform.position.y), Time.deltaTime*200.0f);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GameObject h = Instantiate(hook, hit.point, Quaternion.identity);
+                }
+
+                Vector2 moveTo = new Vector2(hit.point.x, gameObject.transform.position.y);
+                if (moveTo.x < -8)
+                {
+                    moveTo.x = -8;
+                }
+                else if (moveTo.x > 8)
+                {
+                    moveTo.x = 8;
+                }
+                //Debug.Log(moveTo);
+                gameObject.transform.position = Vector2.MoveTowards(transform.position, moveTo, Time.deltaTime * 11.0f);
+                //Debug.Log("Ray Hit");
+            }
+
+
+
+            //mousePos = new Vector2(SceneView.currentDrawingSceneView.camera.pixelWidth-Input.mousePosition.x, SceneView.currentDrawingSceneView.camera.pixelHeight - Input.mousePosition.y);
+            //Debug.Log(mousePos);
+            //transform.position = Vector2.MoveTowards(transform.position, new Vector2(mousePos.x, transform.position.y), Time.deltaTime*200.0f);
+        }
     }
 }
