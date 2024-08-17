66a93890aa5fa42bda3d2687e5394990ed21a92f
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2024-08-18 00:47:19 +1000

    Minefield audio and end

--- Assets/Scripts/Minefield/PlayerController.cs
+++ Assets/Scripts/Minefield/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 
@@ -15,10 +16,17 @@ namespace Milo.Minefield
         public LayerMask whatIsBarriers;
 
         public MineControl mineControl;
+        private AudioSource step;
+        private bool gameComplete;
+        private AudioSource exploadSound;
+        private int amountHit = 0;
+        [SerializeField] private GameObject endGameObject;
         // Start is called before the first frame update
         void Start()
         {
             movePoint.parent = null;
+            step = GetComponents<AudioSource>()[0];
+            exploadSound = GetComponents<AudioSource>()[1];
         }
 
         // Update is called once per frame
@@ -34,6 +42,7 @@ namespace Milo.Minefield
                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatIsBarriers))
                     {
                         movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                        step.Play();
                     }
                 }
 
@@ -42,6 +51,7 @@ namespace Milo.Minefield
                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatIsBarriers))
                     {
                         movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                        step.Play();
                     }
                 }
             }
@@ -54,7 +64,31 @@ namespace Milo.Minefield
                 movePoint.position = new Vector3(5.5f, -4.5f, -1);
                 mineControl.DisableMine(hit);
                 hit = null;
+                amountHit += 1;
+                exploadSound.Play();
             }
+
+            if (!gameComplete)
+            {
+                if (Vector3.Distance(transform.position, new Vector3(-5.5f, 4.5f, -1f)) < 0.1f)
+                {
+
+                    gameComplete = true;
+                    int ticketsGained = 20 - amountHit;
+                    Instantiate(endGameObject);
+                    GameObject.FindGameObjectWithTag("AwardTickets").GetComponent<TextMeshProUGUI>().text = ticketsGained.ToString() + " Tickets Awarded";
+                    GameObject.FindGameObjectWithTag("DataStorage").GetComponent<DataStorage>().tickets += ticketsGained;
+
+                }
+                else if (GameObject.FindGameObjectWithTag("timer").GetComponent<Timer>().timePassed >= GameObject.FindGameObjectWithTag("timer").GetComponent<Timer>().gameTime)
+                {
+                    gameComplete = true;
+                    Instantiate(endGameObject);
+                    GameObject.FindGameObjectWithTag("AwardTickets").GetComponent<TextMeshProUGUI>().text = "0 Tickets Awarded";
+                }
+            }
+
+            
         }
     }
 }
